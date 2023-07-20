61867b89801a0204a471e72f5ec613cef467c5f6
Bonnie <luk23bonnie8@gmail.com>
2023-07-20 22:50:01 +0300

    gamecard

--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import apiClient from '../services/api-client'
 
+export interface Platform {
+id:number,
+name: string,
+slug: string
+}
+
 export interface Games {
     id: number,
     name: string,
     background_image: string
+    parent_platforms: {platform: Platform}[]
     }
     
 interface FetchGamesResponse {
