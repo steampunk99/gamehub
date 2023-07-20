432d7989aa950da7fbabae1bc64893619689061b
Bonnie <luk23bonnie8@gmail.com>
2023-07-20 22:49:56 +0300

    gamecard

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,15 @@
+import { Text } from '@chakra-ui/react'
+import { Platform } from '../hooks/UseGames'
+
+interface Props {
+    platforms: Platform[]
+}
+const PlatformIconList = ({platforms}:Props) => {
+  return (
+    <>
+      {platforms.map((platform)=> <Text>{platform.name}</Text>)}
+    </>
+  )
+}
+
+export default PlatformIconList
