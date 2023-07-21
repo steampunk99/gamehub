bf0017c430f1c2f3cd820e6f8fdae2a333f64316
Bonnie <luk23bonnie8@gmail.com>
2023-07-21 18:59:59 +0300

    optimize images

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,9 @@ import theme from './theme.ts'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
     </ChakraProvider>
-  </React.StrictMode>
+
 )
